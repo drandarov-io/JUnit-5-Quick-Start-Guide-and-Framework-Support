6bb2c172ee79e5a4717b5f1af6b91bed1053fa08
Dmitrij Drandarov <dmitrij.drandarov@outlook.com>
2017-08-02 01:07:34 +0200

    Create JUnit5_06_TestingWithSpring5.java

---
+++ src/test/java/com/drandarov/junit5/JUnit5_06_TestingWithSpring5.java
@@ -0,0 +1,22 @@
+package com.drandarov.junit5;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ *
+ * @author dmitrij-drandarov
+ * @since XX XXX 2017
+ */
+class JUnit5_06_TestingWithSpring5 {
+
+    /*##############################################
+    #           xxxxxxxxxxxxxxxxxxxxxx
+    ##############################################*/
+
+    /**
+     *
+     */
+    @Test
+    void x() {}
+
+}
