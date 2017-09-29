4feee8e64fb13aceacbd5249156ba93e4adcffe6
Dmitrij Drandarov <dmitrij.drandarov@msg.group>
2017-09-29 09:30:54 +0200

    Update Spring 5 to GA, Update Spring Boot 5 to M4, Fix Gradle JUnit Configuration, Update Gradle to 4.2

--- 03-spring/03-02-spring-boot-5/src/test/java/com/dmitrijdrandarov/junit5/SpringBoot5_02_JUnit5Test.java
+++ 03-spring/03-02-spring-boot-5/src/test/java/com/dmitrijdrandarov/junit5/SpringBoot5_02_JUnit5Test.java
@@ -8,7 +8,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.jdbc.EmbeddedDatabaseConnection;
+import org.springframework.boot.jdbc.EmbeddedDatabaseConnection;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
