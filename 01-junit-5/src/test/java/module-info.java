8cdf80ac5181467bfb37d3e14ef585449cfce79a
dmitrij-drandarov <dmitrij.drandarov@outlook.com>
2018-10-18 00:27:13 +0200

    Fix JDK 11 Bugs

---
+++ 01-junit-5/src/test/java/module-info.java
@@ -0,0 +1,9 @@
+module com.dmitrijdrandarov {
+    requires org.junit.jupiter.api;
+    requires javafx.graphics;
+    requires javafx.base;
+    requires javafx.fxml;
+    requires slf4j.api;
+    requires java.sql;
+    requires junit;
+}
